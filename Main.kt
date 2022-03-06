826795a36c930d6771ca6401f33ddcccd57e2986
SzymonMs <szymon.murawski@student.put.poznan.pl>
2022-03-06 21:25:22 +0100

    Update Main.kt

--- Main.kt
+++ Main.kt
@@ -1,3 +1,4 @@
+import javax.swing.text.StyledEditorKit.BoldAction
 import kotlin.math.*
 
 fun fill_progressive(n:Int):List<Int>{
@@ -59,9 +60,73 @@ fun is_prime(n:Int,m:Int):List<Int>{
     }
     return list
 }
+fun pi_liebniz(stop_at:Double):Double{
+    var pi=4.0
+    var i=1
+    while(1.0/i>stop_at){
+        pi=pi+4*Math.pow(-1.0,i.toDouble())*1.0/(2*i+1)
+        i++
+    }
+
+    return pi
+}
+fun draw_square(a:Int) {
+    for (i in 0..a - 1) {
+        for (j in 0..a - 1) {
+            if (i == 0 || i == a - 1|| j==0 || j==a-1) {
+                print("#")
+            }
+            else{
+                print(" ")
+            }
+        }
+        print("\n")
+    }
+}
+fun draw_square2(a:Int,left_diagonal: Boolean,right_diagonal:Boolean){
+    if(left_diagonal==true&&right_diagonal==false){
+        for(i in 0..a-1){
+            for(j in 0..a-1){
+                if(i==0||i==a-1||j==0||j==a-1||i==j){
+                    print("#")
+                }else{
+                    print(" ")
+                }
+            }
+            print("\n")
+        }
+    }
+    if(left_diagonal==true&&right_diagonal==true){
+        for(i in 0..a-1){
+            for(j in 0..a-1){
+                if(i==0||i==a-1||j==0||j==a-1||i==j||i+j==a-1){
+                    print("#")
+                }else{
+                    print(" ")
+                }
+            }
+            print("\n")
+        }
+    }
+}
+fun Euklied(a:Int,b:Int):Int{
+    var c=0
+    var A=a
+    var B=b
+    while(B!=0){
+        c= A.mod(B)
+        A=B
+        B=c
+    }
+    return A
+}
+
 fun main(){
     var l=fill_progressive(12)
     println(l)
     val (min,max)= findMinMax(l)
     println("$min, $max")
+    println(pi_liebniz(0.000001))
+    draw_square2(8,true,true)
+    println(Euklied(4,3))
 }
